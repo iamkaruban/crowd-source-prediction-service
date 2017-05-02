03ddfc821fcfc6b2338214531bd0dad218548551
Calvin Ferrando <karuban.ferurando@gmail.com>
2017-05-03 02:18:42 +0800

    fix reqlite spawning and killing during testing

--- server/test/index.js
+++ server/test/index.js
@@ -1,5 +1,12 @@
+/* eslint global-require: 0 */
+const spawn = require('child_process').spawn;
 // require babel require hook
 require('babel-core/register');
-// require main tests
-require('./main');
-
+// reqlite instance
+const reqlite = spawn('reqlite');
+// wait for start
+reqlite.stderr.on('data', () => {
+  // require main tests
+  const startTests = require('./main').default;
+  startTests(reqlite);
+});
