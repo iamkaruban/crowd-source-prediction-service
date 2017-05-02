1902e45b830d4cdecd0ed7f0392b08397e4b15ff
Calvin Ferrando <karuban.ferurando@gmail.com>
2017-05-02 11:33:20 +0800

    use require hook for tests instead of babel-node

--- server/test/index.js
+++ server/test/index.js
@@ -1,21 +1,5 @@
-// npm packages
-import test from 'tape';
-import request from 'supertest';
+// require babel require hook
+require('babel-core/register');
+// require main tests
+require('./main');
 
-// our packages
-import app from '../src/app';
-
-test('GET /', (t) => {
-  request(app)
-    .get('/')
-    .expect(200)
-    .expect('Content-Type', /text\/html/)
-    .end((err, res) => {
-      const expectedBody = 'Hello world!';
-      const actualBody = res.text;
-
-      t.error(err, 'No error');
-      t.equal(actualBody, expectedBody, 'Retrieve body');
-      t.end();
-    });
-});
