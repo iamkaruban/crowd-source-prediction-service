4a5cfbbecd239bf6af814f19a59ac6a3e7d5ea85
Calvin Ferrando <karuban.ferurando@gmail.com>
2017-05-02 23:37:16 +0800

    add passport.js and setup basic auth methods

--- server/config.js
+++ server/config.js
@@ -3,3 +3,8 @@ export const db = {
   port: 28015,
   db: 'expertsdb',
 };
+
+export const auth = {
+  passwordSalt: 'fmDfeQ2Z8UJHIEf8hn4wCjMbectnqbXU7B2SgeTDqnR1JAWrMXwVwOL5FQ9Xk4F7',
+  sessionSecret: 'Vor8Wvw5Ah5cneqm2Qb5Uce2z4aJscw5zx2RlKHOBG6nbWQo45xv8E9QTac9gCI2',
+};
